0eed7005bea12bd332f2b4dbafad3b456d7d6b9e
hongdmcs <127184611+hongdmcs@users.noreply.github.com>
2023-07-14 14:52:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ recommendBtn.addEventListener('click', () => {
         .map(spicy => spicy.value);
 
     if (selectedCategories.length === 0 || selectedSpiciness.length === 0) {
-        resultDiv.textContent = '종류또는 맵기를 선택해주세요.';
+        resultDiv.textContent = '종류 또는 맵기를 선택해주세요.';
     } else {
         const category = getRandomElement(selectedCategories);
         const spice = getRandomElement(selectedSpiciness);
         const food = getRandomFood(category, spice);
-        resultDiv.textContent = `${food} 추천드립니다! 맛있겠쥬? 츄릅`;
+        resultDiv.textContent = `${food}을(를) 추천드립니다! 맛있겠죠!`;
     }
 });
 
@@ -37,13 +37,15 @@ const menu = {
         rice: ['볶음밥', '김밥', '주먹밥', '카레라이스', '유부초밥', '초밥'],
         bread: ['햄버거', '토스트', '떡국', '떡만둣국'],
         noodle: ['라면', '짜장면', '국수', '냉면', '파스타'],
-        fry: ['치킨', '돈까스', '부추전', '튀김만두', '멘보샤']
+        fry: ['치킨', '돈까스', '부추전', '튀김만두', '멘보샤'],
+        side: ['김치', '오이무침', '간장게장', '계란말이', '시금치나물']
     },
     spicy: {
         soup: ['김치찌개', '순두부찌개', '마라탕(샹궈)'],
         rice: ['김치복음밥', '제육덮밥', '짬뽕밥'],
         bread: ['피자', '떡꼬치', '떡볶이'],
         noodle: ['라면', '짬뽕', '비빔면'],
-        fry: ['칠리새우', '양념치킨', '김치전']
+        fry: ['칠리새우', '양념치킨', '김치전'],
+        side: ['마약계란', '어묵볶음', '애호박볶음', '고추장무침', '건파래볶음']
     }
 };
